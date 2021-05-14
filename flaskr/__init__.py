3646ec071e676ed420231f4ee601f45606e52ff8
renantescaro <renantescaro@yahoo.com.br>
2021-05-14 13:45:14 -0300

    movendo arquivo inicial para pasta flaskr

---
+++ flaskr/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flaskr.routes.rotas import Rotas
+
+db = SQLAlchemy()
+
+def create_app(test_config=None):
+    app = Flask(
+        __name__,
+        static_url_path = '/static',
+        static_folder   = 'static' )
+
+    app.config.from_mapping(
+        SECRET_KEY   = 'super secret key',
+        SESSION_TYPE = 'filesystem',
+        JSONIFY_PRETTYPRINT_REGULAR = False,
+        SQLALCHEMY_DATABASE_URI     = 'mysql://root:Tz0rKy2003@localhost/teste' )
+
+    db.init_app(app)
+
+    Rotas(app)
+    return app
